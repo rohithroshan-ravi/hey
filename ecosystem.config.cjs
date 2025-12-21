249ecb60d880fe3396cab24da9b149fb26180906
Yoginth <hey@yoginth.com>
2025-12-21 21:00:35 +0530

    chore: update ecosystem configuration for production environment paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       args: "start",
-      cwd: "/home/yoginth/hey/apps/web",
+      cwd: "/Users/server/www/hey.xyz",
       env: {
         NODE_ENV: "production"
       },
       max_restarts: 10,
       name: "hey.xyz",
       restart_delay: 4000,
-      script: "/home/yoginth/.nvm/versions/node/v22.21.1/bin/pnpm"
+      script: "/Users/server/.nvm/versions/node/v22.21.1/bin/pnpm"
     }
   ]
 };
